6b5e437401bc371291cac9281553cbb6db700c1b
FlyLive <1585213801@qq.com>
2017-07-08 09:54:54 +0800

    Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index' # 首页
+
   resources :users
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
