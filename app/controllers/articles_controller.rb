0aa47bc30823bdf5401a6d07a6d13ee78f895561
FlyLive <1585213801@qq.com>
2017-07-08 16:05:41 +0800

    Articles CRUD & Article.Comments CR

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret', except: [:index, :show]
+
   def index
     @article = Article.all
   end
@@ -11,7 +13,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  # POST /users
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -20,6 +25,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if@article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
