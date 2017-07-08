b9bbe82ff259577a01493d626fdba08637dc0480
FlyLive <1585213801@qq.com>
2017-07-08 14:03:28 +0800

    Create & Read

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,29 @@
 class ArticlesController < ApplicationController
-  def new
+  def index
+    @article = Article.all
   end
 
-  def create
-    render plain: params[:article].inspect
+  def show
+    @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  # POST /users
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title,:text)
+    end
 end
